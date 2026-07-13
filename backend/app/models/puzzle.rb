51d92d64be0282d7b3371b75f2cecc9a472e60f1
ElCresp0 <jacky8203@protonmail.com>
2026-07-13 18:58:58 +0200

    add User and Puzzle DB tables

---
+++ backend/app/models/puzzle.rb
@@ -0,0 +1,7 @@
+class Puzzle < ApplicationRecord
+  belongs_to :user
+
+  # validations
+  validates :title, presence: true, length: { minimum: 3, maximum: 200 }
+  validates :pgn, presence: true
+end
