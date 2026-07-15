15f493a8c7a1ad20fef3c3b01df42cc32ae7434b
ElCresp0 <jacky8203@protonmail.com>
2026-07-15 22:59:24 +0200

    scaffold puzzle, user, auth models and controllers

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,2 @@
 class User < ApplicationRecord
-    has_secure_password
-
-    # validations
-    validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 100 }
-    validates :password, length: { minimum: 8 }, if: -> { new_record? || !password.nil? }
 end
