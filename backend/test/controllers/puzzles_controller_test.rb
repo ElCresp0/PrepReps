353677c71c419baaf40cd53cbbe0553d53523659
ElCresp0 <jacky8203@protonmail.com>
2026-08-10 22:03:24 +0200

    [backend] add unit tests for PATCH puzzle and GET puzzle by id

--- backend/test/controllers/puzzles_controller_test.rb
+++ backend/test/controllers/puzzles_controller_test.rb
@@ -9,7 +9,7 @@ class PuzzlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   # get puzzles
-  test "should fetch one puzzle" do
+  test "should fetch all puzzles (def in fixtures)" do
     # as defined in puzzles.yml fixture
     get "/puzzles", headers: { Authorization: "Bearer #{@token}" }, as: :json
 
@@ -36,6 +36,21 @@ class PuzzlesControllerTest < ActionDispatch::IntegrationTest
 
   end
 
+  # get one puzzle by id
+  test "should get one puzzle" do
+    first_puzzle = puzzles[0]
+    puzzle_id = first_puzzle["id"]
+    puzzle_title = first_puzzle["title"]
+
+    get "/puzzles/#{puzzle_id}", headers: { Authorization: "Bearer #{@token}" }, as: :json
+
+    assert_response :ok
+
+    data = JSON.parse(response.body)
+    assert data["id"] == puzzle_id
+    assert data["title"] == puzzle_title
+  end
+
   # post a puzzle
   test "should create a puzzle" do
     assert_difference("Puzzle.count") do
@@ -51,6 +66,42 @@ class PuzzlesControllerTest < ActionDispatch::IntegrationTest
     end
     assert_response :conflict
   end
+
+  # update a puzzle title (success)
+  test "should update title and pgn of the first puzzle" do
+    get "/puzzles", headers: { Authorization: "Bearer #{@token}" }, as: :json
+    assert_response :ok
+    data = JSON.parse(response.body)
+    first_puzzle = data["puzzles"][0]
+
+    puzzle_id = first_puzzle["id"]
+    puzzle_title = first_puzzle["title"]
+    new_puzzle_title = puzzle_title + "_UPDATED"
+    new_puzzle_pgn = first_puzzle["pgn"] + "\n\n"
+
+    patch "/puzzles/#{puzzle_id}", headers: { Authorization: "Bearer #{@token}" }, params: { title: new_puzzle_title, pgn: new_puzzle_pgn }, as: :json
+    
+    assert_response :ok
+    data = JSON.parse(response.body)
+    assert data["id"] == puzzle_id
+    assert data["title"] == new_puzzle_title
+    assert data["pgn"] == new_puzzle_pgn
+
+    get "/puzzles/#{puzzle_id}", headers: { Authorization: "Bearer #{@token}" }, as: :json
+
+    assert_response :ok
+    data = JSON.parse(response.body)
+    assert data["id"] == puzzle_id
+    assert data["title"] == new_puzzle_title
+    assert data["pgn"] == new_puzzle_pgn
+
+  end
+  
+  # update a puzzle (unauthorized)
+
+  # update a puzzle title (duplicate title)
+
+  # update a puzzle title (puzzle not found)
 
   # delete a puzzle
   test "should delete the first puzzle" do
