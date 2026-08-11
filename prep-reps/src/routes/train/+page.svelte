10378deb4489bccc8aa48750e50df524d47ae56f
ElCresp0 <jacky8203@protonmail.com>
2026-08-11 22:20:45 +0200

    [frontend] migrate chess logic from component to a typescript class

--- prep-reps/src/routes/train/+page.svelte
+++ prep-reps/src/routes/train/+page.svelte
@@ -3,6 +3,8 @@
     import type { PageProps } from "./$types";
 	import ChessgroundPuzzle from "../../components/ChessgroundPuzzle.svelte";
 	import { Puzzle } from "../../utils/Puzzle";
+    import ChessgroundControls from "../../components/ChessgroundControls.svelte";
+    import { goto } from "$app/navigation";
 
 
 	let { data }: PageProps = $props();
@@ -10,11 +12,64 @@
 	let puzzleIndex = $state(0);
 	let puzzle = $derived((data.puzzles.length > puzzleIndex) ? new Puzzle(data.puzzles[puzzleIndex].title, data.puzzles[puzzleIndex].pgn, data.puzzles[puzzleIndex].id) : null);
 	let chessGroundSize = "512px";
+	let move = $state(0);
 
 	function nextPuzzle() {
 		puzzleIndex++;
 		console.debug("next puzzle");
 	}
+
+    function deletePuzzle() {
+        goto(resolve(puzzle ? `/train?delete=1&puzzle_id=${puzzle.id}` : '/train'));
+        nextPuzzle();
+    }
+
+	function firstMove() {
+		move = 0;
+	}
+
+	function prevMove() {
+		move = move <= 0 ? 0 : move - 1;
+	}
+
+	function nextMove() {
+		if (puzzle !== null && move >= puzzle.moves.length) {
+			move = puzzle.moves.length;
+		}
+		else {
+			move++;
+		}
+	}
+
+	function lastMove() {
+		if (puzzle !== null) {
+			move = puzzle.moves.length;
+		}
+	}
+
+	let chessgroundButtons = $derived(
+        data.signed_in ? [
+            {onclick: firstMove, label: "<<"}, // &laquo;
+            {onclick: prevMove, label: "<"}, //&#8249;
+            {onclick: nextMove, label: ">"}, // &#8250;
+            {onclick: lastMove, label: ">>"}, // &raquo;
+            {dropdown:
+                [
+                    {onclick: deletePuzzle, label: "Delete"},
+                    // {onclick: editPuzzle, label: "Edit"},
+                ],
+                label: "mdi-settings",
+            },
+            {onclick: nextPuzzle, label: "Next"},
+        ]
+        : [
+            {onclick: firstMove, label: "<<"}, // &laquo;
+            {onclick: prevMove, label: "<"}, //&#8249;
+            {onclick: nextMove, label: ">"}, // &#8250;
+            {onclick: lastMove, label: ">>"}, // &raquo;
+            {onclick: nextPuzzle, label: "Next"}
+        ]
+    );
 
 </script>
 
@@ -25,7 +80,8 @@
 {:else if puzzleIndex === data.puzzles.length}
 	<p>That was your last puzzle! Refresh the page to start over.</p>
 {:else}
-	<ChessgroundPuzzle board="blue" pieces="merida" puzzle={puzzle!} nextPuzzleFoo={nextPuzzle} signed_in={data.signed_in} --chessgroundSize={chessGroundSize}/>
+	<ChessgroundPuzzle board="blue" pieces="merida" puzzle={puzzle!} --chessgroundSize={chessGroundSize}/>
+	<ChessgroundControls buttonDefs={chessgroundButtons} --chessgroundSize={chessGroundSize} --buttonsCount={chessgroundButtons.length}/>
 {/if}
 
 <!-- TODO: all puzzles view -->
---
+++ prep-reps/src/utils/ChessgroundPuzzleController.svelte.ts
@@ -0,0 +1,83 @@
+import type { Key } from "@lichess-org/chessground/types";
+import { Puzzle } from "./Puzzle";
+import type { Config } from "@lichess-org/chessground/config";
+import type { Api } from "@lichess-org/chessground/api";
+import { Chess, type NormalMove } from "chessops";
+import { makeBoardFen, parseFen } from "chessops/fen";
+import { moveEquals, opposite, parseSquare } from "chessops/util";
+import { chessgroundMove } from "chessops/compat";
+
+import "$lib/assets/Chessground/chessground.css";
+import "$lib/assets/Chessground/theme.css";
+import { makeSan } from "chessops/san";
+
+    
+export class ChessgroundPuzzleController {
+    private puzzle: Puzzle;
+    private ground: Api;
+    private currentMove: number;
+    private playedMoves: NormalMove[] = [];
+    private fenHistory: string[] = [];
+    private config: Config;
+    private chess: Chess;
+    public currentPgn: string = $state("");
+
+    constructor(puzzle: Puzzle, ground: Api) {
+        this.puzzle = puzzle;
+        console.log(this.puzzle);
+
+        this.chess = Chess.fromSetup(parseFen(puzzle.fen).unwrap()).unwrap();
+        this.config = {
+            "fen": puzzle.fen,
+            "events": {
+                "move": this.userMove,
+            },
+            "movable": {"color": this.chess.turn}
+        };
+
+        this.ground = ground;
+        this.ground.set(this.config);
+        this.playedMoves = [];
+        this.fenHistory = [puzzle.fen];
+        this.currentMove = 0;
+    }
+
+    userMove = (from: Key, to: Key) => {
+        let move = {from: parseSquare(from), to: parseSquare(to)} as NormalMove;
+        console.debug(move, this.puzzle.moves[this.currentMove]);
+        if (moveEquals(this.puzzle.moves[this.currentMove], move) === false) {
+            console.debug("Inorrect move!");
+
+            // ground.cancelMove();
+            this.ground.set({
+                "fen": makeBoardFen(this.chess.board),
+                "turnColor": this.chess.turn,
+                "lastMove": this.playedMoves.length > 0 ? chessgroundMove(this.playedMoves[this.playedMoves.length - 1]) : undefined,
+            });
+        }
+        else {
+            console.debug("Correct move!", makeSan(this.chess, move));
+            this.chess.play(move);
+            this.playedMoves.push(move);
+            this.fenHistory.push(makeBoardFen(this.chess.board));
+            this.currentMove++;
+            console.log(this.chess.turn, this.ground.state.movable.color, this.ground.state.turnColor);
+            if (this.currentMove === this.puzzle.moves.length) {
+                console.debug("Puzzle finished!");
+                this.ground.set({
+                    "viewOnly": true,
+                });
+                // TODO: notify the parent to track progress AND/OR automatically start a new puzzle
+            }
+            else if (this.chess.turn != this.ground.state.movable.color) {
+                let cgMove = chessgroundMove(this.puzzle.moves[this.currentMove]);
+                // ground.move doesn't seem to update the turnColor
+                this.ground.set({"turnColor": opposite(this.chess.turn)});
+                console.debug("auto playing next move, current move:", this.currentMove);
+                this.ground.move(cgMove[0], cgMove[1]);
+                // no need to call chess.play() since ground.move() triggers the move event and goes into this function
+            }
+        }
+        this.currentPgn = this.playedMoves.map((move: NormalMove, i: number): string => makeSan(Chess.fromSetup(parseFen(this.fenHistory[i]).unwrap()).unwrap(), move)).join(" ");
+    }
+}
